f6af8e0a069e5c510eb311a87062227f72c98e68
Lennart Krauch <lennart.krauch.de@gmail.com>
2023-06-14 20:17:21 +0200

    added debug

--- text2img.py
+++ text2img.py
@@ -8,6 +8,8 @@ svc = bentoml.Service("sdv2_1", runners=[stable_diffusion_runner])
 
 @svc.api(input=JSON(), output=Image())
 def txt2img(input_data):
-    # images, _ = stable_diffusion_runner.run(**input_data)
-    images = stable_diffusion_runner.run(**input_data)
+    print(input_data)
+    output = stable_diffusion_runner.run(**input_data)
+    print(output)
+    images, _ = output
     return images[0]
