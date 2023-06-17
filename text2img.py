fc7728daf8a3ce0522dae2322f226f4cbf383c68
Lennart Krauch <lennart.krauch.de@gmail.com>
2023-06-18 01:32:37 +0200

    hotifx

--- text2img.py
+++ text2img.py
@@ -18,10 +18,10 @@ input_spec = JSON(pydantic_model=InputSchema)
 @svc.api(input=input_spec, output=Image())
 def txt2img(input_data: input_spec) -> Image():
     print(input_data)
-    prompt = input_data["prompt"]
-    negative_prompt = input_data["negative_prompt"]
-    num_inference_steps = input_data["num_inference_steps"]
-    guidance_scale = input_data["guidance_scale"]
+    prompt = input_data.prompt
+    negative_prompt = input_data.negative_prompt
+    num_inference_steps = input_data.num_inference_steps
+    guidance_scale = input_data.guidance_scale
     output = stable_diffusion_runner.run(prompt=prompt, negative_prompt=negative_prompt, num_inference_steps=num_inference_steps, guidance_scale=guidance_scale)
     images = output[0]
     return images[0]
