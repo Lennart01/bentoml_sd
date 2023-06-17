33116e332ba7329a9a5ba3f498fa7eba3bda524c
Lennart Krauch <lennart.krauch.de@gmail.com>
2023-06-18 01:24:17 +0200

    added pydantic support

--- text2img.py
+++ text2img.py
@@ -1,12 +1,19 @@
 import bentoml
 from bentoml.io import Image, JSON
+from pydantic import BaseModel
 
 bento_model = bentoml.diffusers.get("sdv2_1:latest")
 stable_diffusion_runner = bento_model.to_runner()
 
 svc = bentoml.Service("sdv2_1", runners=[stable_diffusion_runner])
 
-@svc.api(input=JSON(), output=Image())
+class InputSchema(BaseModel):
+    prompt: str
+    negative_prompt: str
+    num_inference_steps: int
+    guidance_scale: float
+
+@svc.api(input=InputSchema, Output=Image)
 def txt2img(input_data):
     print(input_data)
     output = stable_diffusion_runner.run(**input_data)
